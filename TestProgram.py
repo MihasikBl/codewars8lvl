1b2f41151e59d1cd096ad5d908390bf1fcfea806
MihasikBl <lamagambitor@mail.ru>
2024-12-07 13:44:17 +0300

    first commit

---
+++ TestProgram.py
@@ -0,0 +1,9 @@
+def function_even_or_odd():
+    number = int(input("Please enter your number: "))
+    if number % 2 == 0:
+        print(number, "this is even-number")
+    else:
+        print(number, "this is odd-number")
+
+
+function_even_or_odd()
