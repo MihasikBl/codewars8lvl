71f71cc6cc761294df2b00927d2561d275e7af2d
MihasikBl <lamagambitor@mail.ru>
2025-01-13 08:41:18 +0300

    Update v1

--- Program2.py
+++ Program2.py
@@ -9,5 +9,13 @@ X = input("Please enter your string: ")
 function_for_delete_string_spaces(X)
 
 
+# Alternative solution for CodeWars
+def no_space(x):
+    #your code here
+    string_without_spaces = ""
+    for i in list(x):
+        if i != " ":
+            string_without_spaces += i
+    return string_without_spaces
 
-
+no_space(X)
